8c7280df3353db2e9ea0394766a16787bdf91f62
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 21:59:37 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Recursividade
+{
+    internal class Program 
+    {
+        static int Calcula(int a, int n)
+        {
+            if (n == 0)
+                return 1;
+            else if (a < 0 || n < 0)
+                return -1;
+            else
+                return a * Calcula(a, n-1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de n: ");
+            int n = int.Parse(Console.ReadLine());
+            int resultado = Calcula(a, n);
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
