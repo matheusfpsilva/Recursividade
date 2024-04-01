7cf5a4fa441108e5f8ac1dba9afb8164f8b8dca5
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 22:00:05 -0300

    Create Program2.cs

---
+++ Program2.cs
@@ -0,0 +1,24 @@
+﻿namespace Recursividade
+{
+    internal class Program
+    {
+        static int Soma(int m, int n)
+        {
+            if (n < m)
+                return 0;
+            else
+                return m + Soma(m + 1, n);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de m: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de n: ");
+            int n = int.Parse(Console.ReadLine());
+            int resultado = Soma(m, n);
+            Console.Write(resultado);
+            Console.ReadKey();
+
+        }
+    }
+}
