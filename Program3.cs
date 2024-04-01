bff939e2d4e713f5b746adce750571ad97e4b270
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 22:00:46 -0300

    Create Program3.cs

---
+++ Program3.cs
@@ -0,0 +1,25 @@
+﻿namespace Recursividade
+{
+    internal class Program
+    {
+        static void ParaBinario(double numero)
+        {
+            if (numero < 1)
+                return;
+            else
+            {
+                ParaBinario(numero / 2);
+                int divisao = (int)(numero % 2);
+                Console.Write(divisao);
+            }
+        }
+        static void Main(string[] args)
+        { 
+            Console.Write("Digite um número decimal: ");
+            double numero = double.Parse(Console.ReadLine());
+            Console.Write("Em binário: ");
+            ParaBinario(numero); 
+            Console.ReadKey();
+        }
+    }
+}
