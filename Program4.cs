91af74869e8bd4c0c67c042e3022b874879b9940
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 22:01:04 -0300

    Create Program4.cs

---
+++ Program4.cs
@@ -0,0 +1,24 @@
+﻿namespace Recursividade
+{
+    internal class Program
+    {
+        static int MDC(int x, int y)
+        {
+            if (x == y)
+                return x;
+            else if(x > y)
+                return MDC(x-y, y);
+            else
+                return MDC(x, y - x);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de y: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write(MDC(x, y));
+            Console.ReadKey();
+        }
+    }
+}
